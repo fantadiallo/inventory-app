12d4fa35958e8e3a32a7ccf2687e0e04a32f8e4b
fantadiallo <fatmbo0003@outlook.com>
2025-06-18 15:23:25 +0200

    Create reusable Layout component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
     <>
       <Header />
       <main className="mainContent container py-4">
-        {children}
+        <Outlet />
       </main>
       <Footer />
     </>
