eec1860dec71274dd744636c6360bad3f9ef8321
fantadiallo <fatmbo0003@outlook.com>
2025-06-18 15:23:37 +0200

    Add AdminPage view for admin interface

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,9 +1,18 @@
+import AddInventoryItem from "../components/AddInventoryItem/AddInventoryItem";
+import InventoryList from "../components/InventoryList/InventoryList";
+import LowStockAlert from "../components/LowStockAlert/LowStockAlert";
+import ReviewLogs from "../components/ReviewLogs/ReviewLogs";
+import ShoppingList from "../components/ShoppingList/ShoppingList";
+
+
 export default function AdminPage() {
   return (
-    <div>
-      <h1>Admin Page</h1>
-      <p>This page is for administrators only.</p>
-      {/* Add admin-specific content here */}
-    </div>
+    <main>
+        <ReviewLogs />
+        <LowStockAlert />
+      <AddInventoryItem />
+    <InventoryList />
+<ShoppingList />
+    </main>
   );
 }
