e1ae886139975c1832f64e4c6831474a719f930e
fantadiallo <fatmbo0003@outlook.com>
2025-06-18 15:23:38 +0200

    Add HomePage with dashboard links

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
+import Dashboard from "../components/Dashboard/Dashboard"
+import LogForm from "../components/LogForm/LogForm";
 
 export default function HomePage() {
     return ( 
-<div>
-    <h1>hompage</h1>
-    <p> Welcome to the hompage</p>
-</div>
-
-    )
+     <main>
+    <Dashboard />
+    <LogForm />
+    </main>
+    );
 }
