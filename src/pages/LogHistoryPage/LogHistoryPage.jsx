c5c40163b9fb9422ba6df6c9290734b8924094de
fantadiallo <fatmbo0003@outlook.com>
2025-06-21 16:40:53 +0200

    feat: view history of approved logs by date

---
+++ src/pages/LogHistoryPage/LogHistoryPage.jsx
@@ -0,0 +1,14 @@
+import LogHistory from "../../components/LogHistory/LogHistory";
+
+/**
+ * Page for displaying all approved inventory logs.
+ * @component
+ * @returns {JSX.Element}
+ */
+export default function LogHistoryPage() {
+  return (
+    <main>
+      <LogHistory />
+    </main>
+  );
+}
