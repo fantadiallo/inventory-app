142fb3f5e6d41bf02a38a2b28def34b9ecafa5f9
fantadiallo <fatmbo0003@outlook.com>
2025-06-21 16:41:39 +0200

    feat: implement ShoppingList page for restock planning

---
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,9 @@
+import ShoppingList from "../../components/ShoppingList/ShoppingList";
+
+export default function ShoppingListPage() {
+  return (
+    <main className="container py-4">
+      <ShoppingList />
+    </main>
+  );
+}
